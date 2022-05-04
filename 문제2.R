2d6a43da8cc9b03897565eb2cc2aab87f00c033d
hayaboy <hayaboy@korea.ac.kr>
2022-05-04 15:14:28 +0900

    2022_05_04

---
+++ 문제2.R
@@ -0,0 +1,11 @@
+#통계분석
+
+#1) 종속변수인 Chance_of_Admin(입학 허가 확률)와 독립변수(GRE,TOEFL,Univ_Rating,SOP,LOR,CGPA)에 대해
+#피어슨 상관계수를 이용한 상관관계 분석 수행, 그래프를 이용하여 분석결과 설명
+
+
+#2) GRE,TOEFL,Univ_Rating,SOP,LOR,CGPA, Research가 Chance_of_Admin에 영향을 미치는지 알아보는
+#회귀분석을 단계적 선택법을 사용하여 수행하고 결과 해석
+
+#3) 단계 선택법을 사용해 변수를 선택한 후 새롭게 생성한 회귀 모형에 대한 잔차분석을 수행하고, 그래프
+#를 활용하여 결과 해석
